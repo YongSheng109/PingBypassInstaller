97860d921a527274743532e9fc683a191d5e0d41
YongSheng109 <93895098+YongSheng109@users.noreply.github.com>
2022-10-08 08:02:06 -0700

    Update pingbypasscn.sh

--- pingbypasscn.sh
+++ pingbypasscn.sh
@@ -2,6 +2,7 @@
 apt install python3-pip -y
 apt install iproute2 -y
 apt install screen -y
+cd ~ && wget -q https://github.com/3arthqu4ke/HeadlessMc/releases/download/1.5.2/headlessmc-launcher-1.5.2.jar
 clear
 internalip=$( ip -o route get to 10.0.0.0 | sed -n 's/.*src \([0-9.]\+\).*/\1/p' ) # the ip ok
 javadir=~/jdk8u345-b01/bin
@@ -75,7 +76,6 @@ if [ ! -d "$modsdir" ]; then
 	wget -q https://github.com/3arthqu4ke/3arthh4ck/releases/download/1.8.4/3arthh4ck-1.8.4-release.jar && mv 3arthh4ck-1.8.4-release.jar ~/.minecraft/mods
 	wget -q https://github.com/3arthqu4ke/HMC-Specifics/releases/download/1.0.3/HMC-Specifics-1.12.2-b2-full.jar && mv HMC-Specifics-1.12.2-b2-full.jar ~/.minecraft/mods
 	wget -q https://github.com/3arthqu4ke/HeadlessForge/releases/download/1.2.0/headlessforge-1.2.0.jar && mv headlessforge-1.2.0.jar ~/.minecraft/mods
-	wget -q https://github.com/3arthqu4ke/HeadlessMc/releases/download/1.5.2/headlessmc-launcher-1.5.2.jar
 	wget -q https://github.com/lordofwizard/mcserver/raw/main/startAfk
 	echo '模组下载完成!'
 	sleep 2
@@ -115,7 +115,6 @@ pip3 install pynput
 pip3 install bpytop
 ./startAfk
 clear
-cd ~ && wget -q https://github.com/3arthqu4ke/HeadlessMc/releases/download/1.5.2/headlessmc-launcher-1.5.2.jar
 screen -S server -d -m jdk8u345-b01/bin/java -jar headlessmc-launcher-1.5.2.jar --command launch 0 -id
 screen -S playit -d -m ./playit
 screen -S afk2 -d -m python3 /usr/local/lib/python3.9/dist-packages/bpytop.py
