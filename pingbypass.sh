7a191f5518fc3c4bb750417d38a91ab72867bcf7
SoftWaren1 <150448678+SoftWaren1@users.noreply.github.com>
2023-12-11 09:17:46 +0530

    Update pingbypass.sh

--- pingbypass.sh
+++ pingbypass.sh
@@ -112,7 +112,7 @@ pip3 install bpytop
 ./startAfk
 clear
 screen -S server -d -m jdk8u345-b01/bin/java -jar headlessmc-launcher-1.5.2.jar --command launch 0 -id
-./playit-linux-amd64
+screen -S playit-linux-amd64 -d -m ./playit-linux-amd64
 screen -S afk2 -d -m python3 /usr/local/lib/python3.9/dist-packages/bpytop.py
 screen -S afk -d -m python3 keep-presence/src/keep-presence.py --seconds 30 && cd ~
 notify-send -t 0 $internalip
